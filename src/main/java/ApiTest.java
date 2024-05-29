bb5f2166e3de2fe988ccc6a0eec0d8d58f5928c5
kunre <kunre@Kunren06>
2024-05-29 15:24:11 +0900

    apiテストファイルで動作確認

--- src/main/java/ApiTest.java
+++ src/main/java/ApiTest.java
@@ -6,6 +6,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -19,11 +21,33 @@ public class ApiTest {
 	public static void main(String[] args) throws Exception {
 		ApiTest api = new ApiTest();
 		String response = api.run("https://crd.ndl.go.jp/api/refsearch?type=reference&query=question%20any%20%E8%AA%AD%E6%9B%B8");
+//		https://crd.ndl.go.jp/api/refsearch?type=reference&query=question%20any 
+//		https://crd.ndl.go.jp/api/refsearch?type=reference&query=question%20all
         Document doc = api.convertStringToXMLDocument(response);
         
-        org.w3c.dom.Element root = doc.getDocumentElement();
-        System.out.println("Root element :" + root.getNodeName());
-
+        Element result_set = doc.getDocumentElement();
+        //resultタグ指定でNodelist取得
+        NodeList results = result_set.getElementsByTagName("result");
+        
+        //.item()で一件目のresultを取得
+        Element result = (Element)results.item(0);
+        //referenceタグ指定でElement取得
+        Element reference = (Element) result.getElementsByTagName("reference").item(0);  
+        
+        
+        Element questioneElement = (Element) reference.getElementsByTagName("question").item(0);
+        Element answereElement = (Element) reference.getElementsByTagName("answer").item(0);
+        Element libNameeElement = (Element) reference.getElementsByTagName("lib-name").item(0);
+        
+        
+        String question = questioneElement.getTextContent();
+        String answer = answereElement.getTextContent();
+        String libName = libNameeElement.getTextContent();
+        System.out.println("Root element :" + result_set.getNodeName());
+        System.out.println("質問：" + question);
+        System.out.println("回答：" + answer);
+        System.out.println("提供館：" + libName);
+        
 	}
 	
 	String run(String url) throws IOException {
@@ -37,7 +61,7 @@ public class ApiTest {
 	}
 	
 	// Method to convert String to XML Document
-	private org.w3c.dom.Document convertStringToXMLDocument(String xmlString)
+	private Document convertStringToXMLDocument(String xmlString)
 			throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
