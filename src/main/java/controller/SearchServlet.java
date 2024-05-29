166a8b315c5e4bbd6391c3c21749507e82609a2a
kunre <kunre@Kunren06>
2024-05-29 16:06:55 +0900

    web.xml削除/仮ファイル追加

---
+++ src/main/java/controller/SearchServlet.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SearchServlet() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//リクエストの中身 検索内容、and/orラジオボタン
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
