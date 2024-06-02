689be667f7c9a050d40c57ad61e57ce162618385
Yuichi <117714918+Yuichi0503@users.noreply.github.com>
2024-06-03 01:00:23 +0900

    apiから受け取ったxmlをbeanに変換して返すmodelと関連するcontrollerを更新

--- src/main/java/controller/SearchServlet.java
+++ src/main/java/controller/SearchServlet.java
@@ -8,12 +8,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.ReferenceApi;
 
 /**
  * Servlet implementation class SearchServlet
  */
 
-@WebServlet("/SearchServlet")
+@WebServlet("/search")
 public class SearchServlet extends HttpServlet implements Servlet {
 	private static final long serialVersionUID = 1L;
 
@@ -27,15 +29,47 @@ public class SearchServlet extends HttpServlet implements Servlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String search = null;
+		HttpSession session = request.getSession();
+		
+		String searchText = (String) request.getParameter("searchText");
+		String page = request.getParameter("page");
+		if (page == null) {
+			page = "1";
+		}
+		 
+		String searchTextPage = searchText + page;
+		
+		var refApi = new ReferenceApi();
+		
+		if (null == request.getAttribute(searchTextPage)) {
+			//searchTextとpageを投げて、データを受け取る
+			var rsBean = refApi.getResultSetPageBean(searchText, page);
+			//bean.getResultsCd()で分岐(0:成功、1:失敗)
+			if (rsBean.getResultsCd().equals("0")) {
+				//データをセッションに保存
+				session.setAttribute(searchTextPage, rsBean);
+				//検索結果へフォワード
+				request.setAttribute("searchTextPage", searchTextPage);
+				request.getRequestDispatcher("/result.jsp").forward(request, response);
+			}
+			
+			if (rsBean.getResultsCd().equals("1")) {
+//				1の場合requestScopeにエラーデータを保存してフォワード
+				request.setAttribute("errItem", rsBean.getErrList().getErrItem());
+				request.getRequestDispatcher("/index.jsp").forward(request, response);
+			}
+			
+		}else {
+			//検索結果へフォワード
+			request.setAttribute("searchTextPage", searchTextPage);
+			request.getRequestDispatcher("/result.jsp").forward(request, response);
+		}
 //		検索結果1~25
 //		HashMap<sys-id, bean>
 
-//		ページごとに
-//		ArrayList<HashMap>
 		
 		//リクエストの中身 検索内容、and/orラジオボタン
-		if (search.equals("and")) {
+		if (true) {
 			//and検索をして検索結果をHashMap< sys-id ,HashMap<String,String>  >    >      sessionに保存してfoward
 		}
 		
---
+++ src/main/java/model/ReferenceApi.java
@@ -0,0 +1,78 @@
+package model;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.codec.net.URLCodec;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import model.bean.ResultSetType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ReferenceApi {
+	final static OkHttpClient client = new OkHttpClient();
+	final static URLCodec codec = new URLCodec("UTF-8");
+	
+	public ResultSetType getResultSetPageBean(String searchText, String page) throws UnsupportedEncodingException, IOException{
+		String apiString = "https://crd.ndl.go.jp/api/refsearch?type=reference";
+		String results_num = "&results_num=" + "5";
+		
+		searchText = codec.encode(searchText, "UTF-8");
+		
+		searchText = "&query=question%20any%20" + searchText;
+		page = "&results_get_position=" + page;
+		
+		String xmlString = getUrlResponse(apiString + searchText + page + results_num);
+		
+		try {
+		    // JAXBContextインスタンスを作成
+		    JAXBContext context = JAXBContext.newInstance(ResultSetType.class);
+
+		    // Unmarshallerオブジェクトを作成
+		    Unmarshaller unmarshaller = context.createUnmarshaller();
+
+		    // StringReaderを使用してString型のXMLデータからJavaオブジェクトを生成
+		    @SuppressWarnings("unchecked")
+			JAXBElement<ResultSetType> jaxbElement = (JAXBElement<ResultSetType>) unmarshaller.unmarshal(new StringReader(xmlString));
+		    ResultSetType bean = jaxbElement.getValue();
+
+		    // ここでbeanを使用して何かを行う
+		    // 例：bean.getResult()  List<ResultType>を返す
+		    //     bean.getResult().get(i).getReference() i件目のreferenceを返す
+//		    System.out.println(bean.getResultsCd());
+//		    System.out.println(bean.getResult().get(1).getReference().getQuestionOrRegIdOrAnswer().get(0).getValue());
+//		    System.out.println(bean.getResult().get(0).getReference().getQuestionOrRegIdOrAnswer().get(0).getValue());
+		    return bean;
+
+		} catch (JAXBException e) {
+		    e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+	String getUrlResponse(String url) throws IOException {
+		Request request = new Request.Builder()
+				.url(url)
+				.build();
+
+		Response response = null;
+		try {
+			response = client.newCall(request).execute();
+			return response.body().string();
+		} finally {
+			if (response != null) {
+				response.close();
+			}
+		}
+	}
+
+
+}
