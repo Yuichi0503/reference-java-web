4996e443662a5e97f4e0bf944b4473d1b9b85149
kunre <kunre@Kunren06>
2024-06-05 14:13:22 +0900

    hashとsaltをmapで生成/hash生成

---
+++ src/main/java/model/service/HashService.java
@@ -0,0 +1,48 @@
+package model.service;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashService {
+	
+    private static final SecureRandom random = new SecureRandom();
+	
+	//受け取った値にsaltを追加して、SHA-256でハッシュ化し、その結果をmapで返す
+	static public Map<String, String> hashWithSalt(String password) {
+		//ハッシュ化
+		Map<String, String> hashSaltMap = new HashMap<>();
+		try {
+			String salt = generateSalt();
+			hashSaltMap.put("hash", hash(password + salt) );
+			hashSaltMap.put("salt", salt);
+			return hashSaltMap;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private static String hash(String s) throws Exception {
+		byte[] encodedhash = null;
+		MessageDigest sha256 = null;
+		sha256 = MessageDigest.getInstance("SHA-256");
+		encodedhash = sha256.digest(s.getBytes("UTF-8"));
+		return bytesToHex(encodedhash);
+	}
+
+	private static String generateSalt() {
+		byte[] salt = new byte[16];
+		random.nextBytes(salt);
+		return bytesToHex(salt);
+	}
+	
+	private static String bytesToHex(byte[] inputByte) {
+		BigInteger number = new BigInteger(1, inputByte);
+		StringBuilder hexString = new StringBuilder(number.toString(16));
+		return hexString.toString();
+	}
+}
