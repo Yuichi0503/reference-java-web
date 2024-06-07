0a4a29233868872ceaf789e1eceb69a30aef739f
kunre <kunre@Kunren06>
2024-06-07 10:57:10 +0900

    user_nameフィールドを追加

--- src/main/java/model/bean/UsersEntity.java
+++ src/main/java/model/bean/UsersEntity.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 public class UsersEntity {
 	private String user_id;
+	private String user_name;
 	private String email;
 	private String hashed_password;
 	private String salt;
@@ -21,6 +22,14 @@ public class UsersEntity {
 
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
+	}
+	
+	public String getUser_name() {
+		return user_name;
+	}
+	
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
 	}
 
 	public String getEmail() {
