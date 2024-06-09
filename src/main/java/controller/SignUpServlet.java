1cd8f947f0146a36a2feb5e4126cf04e046fa33a
Yuichi <117714918+Yuichi0503@users.noreply.github.com>
2024-06-09 21:26:28 +0900

    GmailAPIを利用した認証メール自動送信機能を追加

--- src/main/java/controller/SignUpServlet.java
+++ src/main/java/controller/SignUpServlet.java
@@ -1,13 +1,18 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
+import com.google.api.services.gmail.Gmail;
+
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.dao.UsersDao;
+import model.service.EmailService;
 import model.service.UsersService;
 
 /**
@@ -15,6 +20,9 @@ import model.service.UsersService;
  */
 @WebServlet("/signup")
 public class SignUpServlet extends HttpServlet {
+	private static final ResourceBundle bundle = ResourceBundle.getBundle("appConfig");
+	private static final String website = bundle.getString("website");
+	
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -28,14 +36,16 @@ public class SignUpServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
 		String user_name = request.getParameter("user_name");
 		String password = request.getParameter("password");
 		String confirm_password = request.getParameter("confirm_password");
 		String email = request.getParameter("email");
 		
-		if (user_name == null || password == null 
+		if (user_name == null || password == null
 				|| confirm_password == null || email == null
-				|| user_name.equals("") || password.equals("") 
+				|| user_name.equals("") || password.equals("")
 				|| confirm_password.equals("") || email.equals("")) {
 			//エラー処理
 			String msg = "未入力の項目があります";
@@ -43,19 +53,16 @@ public class SignUpServlet extends HttpServlet {
 			//フォワード
 			request.getRequestDispatcher("/register.jsp").forward(request, response);
 		}
-		
 		if (password.equals(confirm_password) == false) {
 			//エラー処理
 			String msg = "パスワードが一致しません";
 			request.setAttribute("msg", msg);
 			//フォワード
 			request.getRequestDispatcher("/register.jsp").forward(request, response);
-			
+
 		}
-		
 		//ユーザーを作成
 		var user = UsersService.createUser(user_name, password, email);
-		
 		//DBに保存
 		if (UsersDao.addEntity(user) == false) {
 			//エラー処理
@@ -64,14 +71,26 @@ public class SignUpServlet extends HttpServlet {
 			//フォワード
 			request.getRequestDispatcher("/register.jsp").forward(request, response);
 		}
-		
-		//メールを送信
-		String website = "http://localhost:8080/reference-java-web";
-		//TODO
-		//String verificationUrl = "website + /verify?token=" + user.getToken();
-        //String message = "以下のリンクをクリックして認証を完了してください\n" + verificationUrl;
-		//sendEmail(user.getEmail(), verificationUrl);
+		//エラーがなければ、認証メールを送信
+		ServletContext servletContext = this.getServletContext();
+		String verificationUrl = website + "/verify?token=" + user.getToken();
+		String message = "以下のアドレスにアクセスして認証を完了してください\n" + verificationUrl;
+		try {
+			Gmail gmail = EmailService.getGmail(servletContext);
+			Gmail.Users gmailUsers = gmail.users();
+			Gmail.Users.Messages gmailMessages = gmailUsers.messages();
+
+			var mimeM = EmailService.createMimeMessage(user.getEmail(), "認証メール", message);
+			var content = EmailService.createMessage(mimeM);
+			var send = gmailMessages.send("me", content);
+			var mRes = send.execute();
+			System.out.println(mRes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		//フォワード
+		request.setAttribute("msg", "認証メールを送信しました");
+		request.getRequestDispatcher("/login.jsp").forward(request, response);
 		
 	}
 
