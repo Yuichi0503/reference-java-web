8751542ba9c50616c92af34ae6fa23109f96bae7
Yuichi <117714918+Yuichi0503@users.noreply.github.com>
2024-06-12 01:44:41 +0900

    WIP

--- src/main/java/model/dao/FavoritesDao.java
+++ src/main/java/model/dao/FavoritesDao.java
@@ -15,4 +15,19 @@ public class FavoritesDao {
 	public static void addFavorite(String userId, String sys_id, ResultSetType bean) {
 		//TODO addFav
 	}
+	
+	//TODO データベースからお気に入りを取得
+	//呼び出し元へsys_id文字列の配列を返す
+	//servletはsys_idをwebAPIに渡し、beanを取得する
+	//servletはbeanをjspに渡す
+	//jspはbeanを表示する
+	//resultをforeachで回して表示
+	//favdetail.jspに遷移するリンクを表示
+	
+	
+//	public List<Favorite> getFavoritesByUserId(String userId) {
+//	    // SQLを実行してお気に入りを取得
+//	-- ユーザーのお気に入りを取得
+//	SELECT * FROM favorites WHERE user_id = ? ORDER BY saved_at DESC;
+//	}
 }
--- src/main/java/model/entity/FavoritesEntity.java
+++ src/main/java/model/entity/FavoritesEntity.java
@@ -1,5 +1,81 @@
 package model.entity;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+import model.ReferenceApi;
+import model.bean.ResultSetType;
+
 public class FavoritesEntity {
+	
+	//メインメソッド
+	public static void main(String[] args) {
+		new FavoritesEntity().test();
+	}
+	
 	//TODO FavEntity beanをシリアライズ
+	public void test() {
+		
+		var bean = new ReferenceApi().getBeanBySys_id("1000205483 1000113444");
+	    String json = serializeWithJackson(bean);
+	    System.out.println(json);
+//	    ResultSetType bean2 = deserializeWithJackson(json);
+//	    System.out.println(bean2);
+	}
+	
+	public void serializeBean(Object bean, String filePath) {
+	    try (FileOutputStream fileOut = new FileOutputStream(filePath);
+	         ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+	        out.writeObject(bean);
+	        System.out.println("Serialized data is saved in " + filePath);
+	    } catch (IOException i) {
+	        i.printStackTrace();
+	    }
+	}
+	public ResultSetType deserializeBean(String filePath) {
+	    Object bean = null;
+	    try (FileInputStream fileIn = new FileInputStream(filePath);
+	         ObjectInputStream in = new ObjectInputStream(fileIn)) {
+	        bean = in.readObject();
+	        System.out.println("Deserialized data is read from " + filePath);
+	    } catch (IOException i) {
+	        i.printStackTrace();
+	    } catch (ClassNotFoundException c) {
+	        System.out.println("Class not found");
+	        c.printStackTrace();
+	    }
+	    return (ResultSetType) bean ;
+	}
+	public String serializeWithGson(Object object) {
+	    Gson gson = new Gson();
+	    String json = gson.toJson(object);
+	    return json;
+	}
+	public String serializeWithJackson(Object object) {
+	    ObjectMapper mapper = new ObjectMapper();
+	    String json = "";
+	    try {
+	        json = mapper.writeValueAsString(object);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return json;
+	}
+	
+	public ResultSetType deserializeWithJackson(String json) {
+	    ObjectMapper mapper = new ObjectMapper();
+	    ResultSetType bean = null;
+	    try {
+	        bean = mapper.readValue(json, ResultSetType.class);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return bean;
+	}
 }
