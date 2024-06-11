31ccdf28f360bb3d7a18b5fac34dc6918d287490
Yuichi <117714918+Yuichi0503@users.noreply.github.com>
2024-06-11 09:47:31 +0900

    isAllowedRequestで管理するように修正/verifyをアクセス可能に

--- src/main/java/filter/AuthenticationFilter.java
+++ src/main/java/filter/AuthenticationFilter.java
@@ -13,9 +13,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-/**
- * 全てのリクエストに対する認証フィルター
- */
 @WebFilter("/*")
 public class AuthenticationFilter implements Filter {
 
@@ -24,32 +21,24 @@ public class AuthenticationFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpSession session = request.getSession(false);
-		
-		// コンテキストパスとリクエストURIを取得
-		String contextPath = request.getContextPath();
-		String requestURI = request.getRequestURI().substring(contextPath.length());
-		
-		// ログインとサインアップのURIを定義
-		String loginURI = "/login";
-		String loginJspURI = "/login.jsp";
-		String signUpURI = "/signup";
-		String signUpJspURI = "/signup.jsp";
-		
-		// ユーザーがログインしているか、もしくは未ログインでもアクセス可能なリクエスト
-		// (ログイン、サインアップ、CSS、JS)の場合は、フィルターを通過させる
+
+		String requestURI = request.getRequestURI().substring(request.getContextPath().length());
+
 		boolean loggedIn = session != null && session.getAttribute("user_id") != null;
-		boolean loginRequest = requestURI.equals(loginURI);
-		boolean signUpRequest = requestURI.equals(signUpURI);
-		boolean cssRequest = requestURI.endsWith(".css");
-		boolean jsRequest = requestURI.endsWith(".js");
-		
-		//条件に合わなければログインページへフォワードする
-		if (loggedIn || loginRequest || signUpRequest || cssRequest || jsRequest || requestURI.equals(loginJspURI) || requestURI.equals(signUpJspURI)) {
+		boolean isAllowedRequest = isAllowedRequest(requestURI);
+
+		if (loggedIn || isAllowedRequest) {
 			chain.doFilter(request, response);
 		} else {
 			request.setAttribute("msg", "再度ログインしてください。");
 			request.getRequestDispatcher("/login.jsp").forward(request, response);
 		}
+	}
+
+	private boolean isAllowedRequest(String requestURI) {
+		return requestURI.equals("/login") || requestURI.equals("/signup") || requestURI.endsWith(".css")
+				|| requestURI.endsWith(".js") || requestURI.equals("/login.jsp") || requestURI.equals("/signup.jsp")
+				|| requestURI.equals("/verify");
 	}
 
 	// Add init and destroy methods if needed
