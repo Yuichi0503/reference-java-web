799907e43de693162754fb97deb2a1f95acd7e81
Yuichi <117714918+Yuichi0503@users.noreply.github.com>
2024-06-14 01:44:21 +0900

    メソッドの分割

--- src/main/java/controller/SearchServlet.java
+++ src/main/java/controller/SearchServlet.java
@@ -29,60 +29,59 @@ public class SearchServlet extends HttpServlet implements Servlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		final int RESULT_NUM = 25;
-		String searchText = request.getParameter("searchText");
-		String page = request.getParameter("page");
-		if (page == null) {
-			page = "1";
-		}
-		 
-		var refApi = new ReferenceApi();
-		
-		//検索文字列+ページ番号の名前のBeanがセッションにない場合
-		if (null == session.getAttribute(searchText + page)) {
-			//searchTextとpageを投げて、データを受け取る
-			var rsBean = refApi.getResultSetPageBean(searchText, page);
-			//bean.getResultsCd()で分岐(0:成功、1:失敗)
-			if (rsBean.getResultsCd().equals("0")) {
-				//検索文字列+pageでBeanをセッションに保存
-				session.setAttribute(searchText + page, rsBean);
-				
-				int hit_num = Integer.parseInt(rsBean.getHitNum());
-				int totalPages =(hit_num + RESULT_NUM - 1) /RESULT_NUM;
-				//検索文字列+TotalPagesの名前で総ページ数をセッションに保存
-				session.setAttribute(searchText + "TotalPages", totalPages);
-				
-				//検索結果へフォワード
-				request.setAttribute("stringSearchTextTotalPages", searchText + "TotalPages");
-				request.setAttribute("RESULT_NUM", RESULT_NUM);
-				request.setAttribute("searchText", searchText);
-				request.setAttribute("page", Integer.parseInt(page));
-				request.setAttribute("rsBean", rsBean);
-				request.getRequestDispatcher("/result.jsp").forward(request, response);
-			}
-			
-			if (rsBean.getResultsCd().equals("1")) {
-//				1の場合requestScopeにエラーデータを保存してフォワード
-				request.setAttribute("errItem", rsBean.getErrList().getErrItem());
-				request.getRequestDispatcher("/index.jsp").forward(request, response);
-			}
-			
-		}else {
-			//検索文字列+ページ番号の名前のBeanがセッションにある場合
-			var rsBean = (ResultSetType)session.getAttribute(searchText + page);
-			
-			//検索結果へフォワード
-			request.setAttribute("stringSearchTextTotalPages", searchText + "TotalPages");
-			request.setAttribute("RESULT_NUM", RESULT_NUM);
-			request.setAttribute("searchText", searchText);
-			request.setAttribute("page", Integer.parseInt(page));
-			request.setAttribute("rsBean", rsBean);
-			request.getRequestDispatcher("/result.jsp").forward(request, response);
-		}
-		
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        final int RESULT_NUM = 25;
+        String searchText = request.getParameter("searchText");
+        String page = request.getParameter("page");
+        if (page == null) {
+            page = "1";
+        }
+
+        var refApi = new ReferenceApi();
+
+        if (null == session.getAttribute(searchText + page)) {
+            handleNewSearch(request, response, session, RESULT_NUM, searchText, page, refApi);
+        } else {
+            handleExistingSearch(request, response, session, RESULT_NUM, searchText, page);
+        }
+    }
+
+
+    private void handleNewSearch(HttpServletRequest request, HttpServletResponse response, HttpSession session, int RESULT_NUM, String searchText, String page, ReferenceApi refApi) throws ServletException, IOException {
+        var rsBean = refApi.getResultSetPageBean(searchText, page);
+        if (rsBean.getResultsCd().equals("0")) {
+            session.setAttribute(searchText + page, rsBean);
+            int hit_num = Integer.parseInt(rsBean.getHitNum());
+            int totalPages =(hit_num + RESULT_NUM - 1) /RESULT_NUM;
+            session.setAttribute(searchText + "TotalPages", totalPages);
+            setRequestAttributes(request, RESULT_NUM, searchText, page, rsBean);
+            request.getRequestDispatcher("/result.jsp").forward(request, response);
+        }//エラー処理
+        if (rsBean.getResultsCd().equals("1")) {
+            request.setAttribute("errItem", rsBean.getErrList().getErrItem());
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+        }
+    }
+
+    private void handleExistingSearch(HttpServletRequest request, HttpServletResponse response, HttpSession session, int RESULT_NUM, String searchText, String page) throws ServletException, IOException {
+        var rsBean = (ResultSetType)session.getAttribute(searchText + page);
+        setRequestAttributes(request, RESULT_NUM, searchText, page, rsBean);
+        String id = request.getAttribute("id") != null ? request.getAttribute("id").toString() : null;
+        if (id != null) {
+            request.setAttribute("id", id);
+            request.getRequestDispatcher("/result.jsp").forward(request, response);
+        }
+        request.getRequestDispatcher("/result.jsp").forward(request, response);
+    }
+
+    private void setRequestAttributes(HttpServletRequest request, int RESULT_NUM, String searchText, String page, ResultSetType rsBean) {
+        request.setAttribute("stringSearchTextTotalPages", searchText + "TotalPages");
+        request.setAttribute("RESULT_NUM", RESULT_NUM);
+        request.setAttribute("searchText", searchText);
+        request.setAttribute("page", Integer.parseInt(page));
+        request.setAttribute("rsBean", rsBean);
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
