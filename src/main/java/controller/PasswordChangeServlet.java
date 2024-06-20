5bbb2e04f9709677d0c17a04a18b9a9a19d77655
Yuichi <117714918+Yuichi0503@users.noreply.github.com>
2024-06-20 10:07:33 +0900

    usersのhashed_password/salt変更機能を追加

--- src/main/java/controller/PasswordChangeServlet.java
+++ src/main/java/controller/PasswordChangeServlet.java
@@ -7,6 +7,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.dao.UsersDao;
+import model.service.HashService;
 
 /**
  * Servlet implementation class PasswordChangeServlet
@@ -20,22 +22,39 @@ public class PasswordChangeServlet extends HttpServlet {
      */
     public PasswordChangeServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//password_change.jspからパラメーターを取得
+		String password = request.getParameter("password");
+		String confirm_password = request.getParameter("confirm_password");
+		
+		//セッションからuser_idを取得
+		String user_id = (String)request.getSession().getAttribute("user_id");
+		
+		//パスワードの確認
+		if (password == null || !password.equals(confirm_password)) {
+			request.setAttribute("msg", "パスワードが一致しません");
+			request.getRequestDispatcher("/password_change.jsp").forward(request, response);
+			return;
+		}
+		else {
+			//パスワード変更処理
+			var map = HashService.hashWithSalt(password);
+			UsersDao.updatePasswordAndSalt(user_id, map.get("hash"), map.get("salt"));
+			request.setAttribute("msg", "パスワードの変更に成功しました");
+			request.getRequestDispatcher("/mypage.jsp").forward(request, response);
+			return;
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
